f493876f302cb8f19f6736d1e9684eba2c39ecf9
Jessy <bakijessy@gmail.com>
2025-09-28 17:17:57 +0000

    feat: add content security policy middleware for enhanced security

--- myApp.js
+++ myApp.js
@@ -26,14 +26,15 @@ app.use(helmet.dnsPrefetchControl());
 
 app.use(helmet.noCache());
 
-
-
-
-
-
-
-
-
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "trusted-cdn.com"],
+      styleSrc: ["'self'", 'trusted-cdn.com'],
+    },
+  })
+);
 
 
 
