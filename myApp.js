200d9853f1b1de36f584e168de74cb3fb92ba154
Jessy <bakijessy@gmail.com>
2025-09-28 17:11:15 +0000

    feat: add HSTS middleware for enhanced security

--- myApp.js
+++ myApp.js
@@ -13,14 +13,14 @@ app.use(helmet.noSniff());
 
 app.use(helmet.ieNoOpen());
 
+let ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 
-
-
-
-
-
-
-
+app.use(
+	helmet.hsts({
+		maxAge: ninetyDaysInSeconds,
+		force: true,
+	})
+);
 
 
 
